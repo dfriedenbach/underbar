407938ac06933778c0c338dae47654c5eaf5379a
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-05-03 22:18:59 -0700

    filter function meets specifications

--- src/underbar.js
+++ src/underbar.js
@@ -81,6 +81,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(value) {
+      if(test(value)) {
+        result.push(value);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
