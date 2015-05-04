f120c25e0741cc785cdec904770649cf74505d2e
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-05-03 22:02:33 -0700

    last function meets specifications

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length - 1];
+    }
+    var start = (n >= array.length ? 0 : array.length - n);
+    return array.slice(start, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
