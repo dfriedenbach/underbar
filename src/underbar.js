ac684b02e8f6e9599a1ac22948acd9bc8e2f4479
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-05-04 11:17:02 -0700

    some function meets specifications

--- src/underbar.js
+++ src/underbar.js
@@ -199,6 +199,12 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if(iterator === undefined) {
+      iterator = _.identity;
+    }
+    return ! _.every(collection, function(value) {
+      return ! iterator(value);
+    });
   };
 
 
