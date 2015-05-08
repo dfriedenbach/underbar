3580cf3b0a69ac5075d64f64e2325a0fd3cfd91e
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-05-07 23:12:13 -0700

    Refactor delay to use Array.slice on arguments

--- src/underbar.js
+++ src/underbar.js
@@ -317,10 +317,7 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
-    var args = [];
-    for(var i = 2; i < arguments.length; i++) {
-      args.push(arguments[i]);
-    }
+    var args = Array.prototype.slice.call(arguments, 2);
     setTimeout(function() {
       func.apply(this, args);
     }, wait);
